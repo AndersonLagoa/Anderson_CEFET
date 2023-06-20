3ff7bcaeb061a248450c6de0b2d5574b0a10894b
ANDERSON RODRIGUES DOS SANTOS <60968822+AndersonR-S@users.noreply.github.com>
2023-06-20 14:53:50 -0300

    Atualizar o main.c
    a

--- CEFET_Faculdade/1° Semestre/TRABALHO/trabalho 2 IPC/main.c
+++ CEFET_Faculdade/1° Semestre/TRABALHO/trabalho 2 IPC/main.c
@@ -1,6 +1,10 @@
 #include "ponte.h"
-#include <stdio.h>
-#include <string.h>
 int main(){
+ chama_mapa();//chama o mapa do arquivo junto com o tamanho 
+ confere_mapa();// confere se o mapa é de acordo com o tamanho e descobri onde estão os tesouros e o ponto inicial
+ possibilidades_percorre();// quantas maneiras que chegar nos tesouros
+ encontra_caminho();//encontrar os caminhos 
+return 0;
+
  
 }
