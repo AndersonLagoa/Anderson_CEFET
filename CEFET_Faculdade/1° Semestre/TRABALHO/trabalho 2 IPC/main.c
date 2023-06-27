635f1581cb3f327f041e18c99a2e8bfcf171c94b
ANDERSON RODRIGUES DOS SANTOS <60968822+AndersonR-S@users.noreply.github.com>
2023-06-27 14:52:55 -0300

    Atualizar o main.c

--- CEFET_Faculdade/1° Semestre/TRABALHO/trabalho 2 IPC/main.c
+++ CEFET_Faculdade/1° Semestre/TRABALHO/trabalho 2 IPC/main.c
@@ -1,11 +1,11 @@
 #include "central.h"
-int main(){
-Caminhos *possibilidade;
- chama_mapa();//chama o mapa do arquivo junto com o tamanho 
- confere_mapa();// confere se o mapa é de acordo com o tamanho e descobri onde estão os tesouros e o ponto inicial
- possibilidades_percorre();// quantas maneiras que chegar nos tesouros
- encontra_caminho(possibilidade);//encontrar os caminhos 
-return 0;
-
- 
+char *chama_mapa(){
+int linha, coluna;
+FILE *arquivo = fopen("mapa.txt","r");
+If(arquivo==NULL){
+printf("Erro em abrir o arquivo mapa!!\n\n");
+exit(0);
 }
+fscanf(arquivo, "%d","%d",linha, coluna);
+char **mapa = (char **)malloc(linha*sizeof(int *);
+fordaW4
